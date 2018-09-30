eb7bc6eeb17a7e45b18da522f2df9f7aedbafd23
F1LT3R <al@pwn.io>
2018-09-30 00:39:48 -0400

    Try to debug travis path issue.

--- tests/markserv-cli-readme-dir-down.test.js
+++ tests/markserv-cli-readme-dir-down.test.js
@@ -5,7 +5,7 @@ import test from 'ava'
 import getPort from 'get-port'
 import readme from '../lib/readme'
 
-test.cb('start markserv via "readme" command from child dir', t => {
+test.cb('start markserv via "readme" command', t => {
 	t.plan(3)
 
 	const expected = String(
@@ -26,7 +26,15 @@ test.cb('start markserv via "readme" command from child dir', t => {
 			browser: false
 		}
 
+		const done = () => {
+			t.end()
+		}
+
 		readme.run(flags).then(service => {
+			const closeServer = () => {
+				service.httpServer.close(done)
+			}
+
 			const opts = {
 				url: service.launchUrl,
 				timeout: 1000 * 2
@@ -34,16 +42,15 @@ test.cb('start markserv via "readme" command from child dir', t => {
 
 			request(opts, (err, res, body) => {
 				if (err) {
-					service.httpServer.close()
 					t.fail(err)
+					closeServer()
 				}
 
 				t.true(body.includes(expected))
 
 				t.is(res.statusCode, 200)
 				t.pass()
-				service.httpServer.close()
-				t.end()
+				closeServer()
 			})
 		}).catch(err => {
 			t.fail(err)
